782d23fa6d3e652dae7988fe6c4d7614e28d3f7f
manish-jandu <manishjandu1@gmail.com>
2023-06-04 20:47:10 +0530

    todo setup in node server

--- NotesApp.js
+++ NotesApp.js
@@ -10,3 +10,11 @@ app.get('/',(req,res)=>{
 app.listen(port, ()=>{
     console.log(`Running server on ${port}`);
 });
+
+
+//Todo:
+// 1. Add a single Note.
+// 2. Show all Notes List.
+// 3. Delete A Note.
+// 4. Search a note with title.
+// Note(title,desc,_id)
