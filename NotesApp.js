d2c2113239379c1d0fff833fd4dad11fa776454d
manish-jandu <manishjandu1@gmail.com>
2023-06-04 20:44:36 +0530

    express server setup

--- NotesApp.js
+++ NotesApp.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/',(req,res)=>{
+    res.send("this is a response from server")
+});
+
+app.listen(port, ()=>{
+    console.log(`Running server on ${port}`);
+});
